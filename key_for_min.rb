c400f1d5cb7db3b78ce90ef571143977a05e1736
Kimberly Aviles <01010110.v@gmail.com>
2018-11-14 07:39:38 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,20 +3,25 @@
 def key_for_min_value(hash)
 
   val_dump = nil
-  key_dump = []
+  key_dump =
+  
 
   hash.each do |k, v|
+    
     if val_dump == nil
       val_dump = v
     elsif val_dump != nil && v < val_dump
       val_dump = v
-    elsif val_dump == v
-      key_dump << k
     else
       return nil
     end
+    
   end
 
-  return key_dump
+  hash.each do |k, v|
+    k == val_dump
+    return k
+  end
+
 
 end
