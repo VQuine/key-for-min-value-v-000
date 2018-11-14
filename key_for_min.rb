e6af43989e88eabd0b92f9cf27f384a59185e439
Kimberly Aviles <01010110.v@gmail.com>
2018-11-14 06:50:11 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,6 +1,16 @@
 # prereqs: iterators, hashes, conditional logic
 # Given a hash with numeric values, return the key for the smallest value
+def key_for_min_value(hash)
 
-def key_for_min_value(name_hash)
+  val_dump = nil
+  key_dump = {}
+  hash.each do |k, v|
+    val_dump == nil ? val_dump = v : val_dump
+    val_dump != nil && v < val_dump ? val_dump = v : val_dump
+    val_dump == v ? key_dump = k : nil
+  end
+  
+  
+  return key_dump
 
 end
